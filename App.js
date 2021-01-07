9233514a8bc531dba2acea836a3ad601a12bf73e
quetal <gibum1995@gmail.com>
2021-01-07 17:36:34 +0900

    call weather api

--- App.js
+++ App.js
@@ -1,25 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {Alert} from 'react-native';
 import Loading from './Loading';
 import * as Location from 'expo-location';
+import axios from 'axios';
+
+const API_KEY = "2fb1b957d16dbdd5ad66ef3255bdbfee";
 
 export default class extends React.Component{
-  getLocation = async() =>{
+  state ={
+    isLoading : true
+  };
+  getWeather = async(latitude, longitude) => {
+    const {data} = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
+  };
+  getLocation = async() => {
     try {
-      const response = await Location.requestPermissionsAsync();
-      console.log(response);
-      const loation = await Location.getCurrentPositionAsync();
-      console.log(location);
+      Location.requestPermissionsAsync();
+      const {coords :{latitude, longitude}} = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({isLoading:false});
+      
     } catch (error) {
       Alert.alert("cannot find");
     }
-
-  }
+  };
   componentDidMount(){
     this.getLocation();
-  }
+  };
   render(){
-    return <Loading />;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading />  : null;
   }
 } 
